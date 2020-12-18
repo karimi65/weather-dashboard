f0e534bdd85bd61dfbc5de60389897f2b2bb48a9
Captain <71038018+karimi65@users.noreply.github.com>
2020-12-17 23:07:54 -0600

    fix color coding of UV index

--- assets/script.js
+++ assets/script.js
@@ -29,38 +29,40 @@ $("#find-city").on("click", function (event) {
 
 // get current UV index
 function currentUvIndex(lon, lat) {
-
-    var uvqURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&apikey=5548be92d388f7fbaadf4277d9c3c68d";
+    var uvApiKey = "&apikey=5548be92d388f7fbaadf4277d9c3c68d"
+    var uvqURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + uvApiKey;
     $.ajax({
         url: uvqURL,
         method: "GET"
     }).then(function (response) {
-        $(".uvIndex").text(response.value)
+        console.log(response)
+        var UVIndex = response.value
+        $(".uvIndex").text(UVIndex)
+
+        //determine severity of UV Index for color coding
+        var UVbg = null;
+        var textColor = null;
+        if (UVIndex < 2) {
+            UVbg = "green";
+            textColor = "white";
+        } else if (UVIndex < 6) {
+            UVbg = "yellow";
+            textColor = "black";
+        } else if (UVIndex < 8) {
+            UVbg = "orange";
+            textColor = "black";
+        } else if (UVIndex < 11) {
+            UVbg = "red";
+            textColor = "white";
+        } else {
+            UVbg = "violet";
+            textColor = "white";
+        }
+        $(".uvIndex").css("backgroundColor", UVbg)
+            .css("color", textColor);
     })
 
 
-    //determine severity of UV Index for color coding
-    var UVbg = null;
-    var textColor = null;
-    if (currentUvIndex < 2) {
-        UVbg = "green";
-        textColor = "white";
-        severity = "low"
-    } else if (currentUvIndex < 6) {
-        UVbg = "yellow";
-        textColor = "black";
-    } else if (currentUvIndex < 8) {
-        UVbg = "orange";
-        textColor = "black";
-    } else if (currentUvIndex < 11) {
-        UVbg = "red";
-        textColor = "white";
-    } else {
-        UVbg = "violet";
-        textColor = "white";
-    }
-    $(".uvIndex").css("backgroundColor", UVbg)
-        .css("color", textColor);
 }
 
 
